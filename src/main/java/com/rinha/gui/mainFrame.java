a429f43e130446feaa0849302d8d4e37d2a12289
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2023-11-04 23:08:09 -0300

    Add Tela de Batalha
    Criada a base de tela de batalha.

--- src/main/java/com/rinha/gui/mainFrame.java
+++ src/main/java/com/rinha/gui/mainFrame.java
@@ -36,15 +36,30 @@ public class mainFrame extends javax.swing.JFrame {
         botaoPlay = new javax.swing.JButton();
         labelBackgroundTelaInicial = new javax.swing.JLabel();
         telaDashBoard = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        botaoTorneios = new javax.swing.JButton();
         menuInicial = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        botaoLoja = new javax.swing.JButton();
         botaoPerfil = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        botaoPartidaRapida = new javax.swing.JButton();
         botaoGaloDex = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        labelBackgroundDashBoard = new javax.swing.JLabel();
         telaLoja = new javax.swing.JPanel();
         telaBatalha = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        labelPP = new javax.swing.JLabel();
+        jButton6 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jProgressBar2 = new javax.swing.JProgressBar();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("rinha");
@@ -114,22 +129,27 @@ public class mainFrame extends javax.swing.JFrame {
         telaDashBoard.setPreferredSize(getPreferredSize());
         telaDashBoard.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setBackground(new java.awt.Color(255, 135, 30));
-        jButton1.setFont(new java.awt.Font("sansserif", 0, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Torneios");
-        telaDashBoard.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 386, 182, 54));
+        botaoTorneios.setBackground(new java.awt.Color(255, 135, 30));
+        botaoTorneios.setFont(new java.awt.Font("sansserif", 0, 18)); // NOI18N
+        botaoTorneios.setForeground(new java.awt.Color(255, 255, 255));
+        botaoTorneios.setText("Torneios");
+        botaoTorneios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoTorneiosActionPerformed(evt);
+            }
+        });
+        telaDashBoard.add(botaoTorneios, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 386, 182, 54));
 
         menuInicial.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/menuIcon.png"))); // NOI18N
         menuInicial.setAlignmentY(0.0F);
         menuInicial.setContentAreaFilled(false);
         telaDashBoard.add(menuInicial, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 40, 48, 48));
 
-        jButton3.setBackground(new java.awt.Color(75, 150, 187));
-        jButton3.setFont(new java.awt.Font("sansserif", 0, 18)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("Loja");
-        telaDashBoard.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(498, 386, 182, 54));
+        botaoLoja.setBackground(new java.awt.Color(75, 150, 187));
+        botaoLoja.setFont(new java.awt.Font("sansserif", 0, 18)); // NOI18N
+        botaoLoja.setForeground(new java.awt.Color(255, 255, 255));
+        botaoLoja.setText("Loja");
+        telaDashBoard.add(botaoLoja, new org.netbeans.lib.awtextra.AbsoluteConstraints(498, 386, 182, 54));
 
         botaoPerfil.setBackground(new java.awt.Color(228, 56, 82));
         botaoPerfil.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
@@ -138,11 +158,16 @@ public class mainFrame extends javax.swing.JFrame {
         botaoPerfil.setAlignmentY(0.0F);
         telaDashBoard.add(botaoPerfil, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 40, 100, 31));
 
-        jButton5.setBackground(new java.awt.Color(255, 135, 30));
-        jButton5.setFont(new java.awt.Font("sansserif", 0, 18)); // NOI18N
-        jButton5.setForeground(new java.awt.Color(255, 255, 255));
-        jButton5.setText("Partida Rápida");
-        telaDashBoard.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 320, 182, 54));
+        botaoPartidaRapida.setBackground(new java.awt.Color(255, 135, 30));
+        botaoPartidaRapida.setFont(new java.awt.Font("sansserif", 0, 18)); // NOI18N
+        botaoPartidaRapida.setForeground(new java.awt.Color(255, 255, 255));
+        botaoPartidaRapida.setText("Partida Rápida");
+        botaoPartidaRapida.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoPartidaRapidaActionPerformed(evt);
+            }
+        });
+        telaDashBoard.add(botaoPartidaRapida, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 320, 182, 54));
 
         botaoGaloDex.setBackground(new java.awt.Color(228, 56, 82));
         botaoGaloDex.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
@@ -151,10 +176,10 @@ public class mainFrame extends javax.swing.JFrame {
         botaoGaloDex.setAlignmentY(0.0F);
         telaDashBoard.add(botaoGaloDex, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 83, 100, 31));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/dash.png"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        jLabel1.setToolTipText("");
-        telaDashBoard.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 720, 480));
+        labelBackgroundDashBoard.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/dash.png"))); // NOI18N
+        labelBackgroundDashBoard.setText("jLabel1");
+        labelBackgroundDashBoard.setToolTipText("");
+        telaDashBoard.add(labelBackgroundDashBoard, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 720, 480));
 
         painelPrincipal.add(telaDashBoard, "telaDashBoard");
 
@@ -180,17 +205,103 @@ public class mainFrame extends javax.swing.JFrame {
         telaBatalha.setAlignmentY(0.0F);
         telaBatalha.setMaximumSize(getPreferredSize());
         telaBatalha.setMinimumSize(getPreferredSize());
+        telaBatalha.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout telaBatalhaLayout = new javax.swing.GroupLayout(telaBatalha);
-        telaBatalha.setLayout(telaBatalhaLayout);
-        telaBatalhaLayout.setHorizontalGroup(
-            telaBatalhaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+        jLabel2.setText("jLabel2");
+        jLabel2.setMaximumSize(jLabel2.getPreferredSize());
+        jLabel2.setMinimumSize(jLabel2.getPreferredSize());
+        telaBatalha.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 150, 150, 150));
+
+        jPanel1.setBackground(new java.awt.Color(238, 240, 242));
+
+        jButton1.setText("jButton1");
+
+        jButton2.setText("jButton2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("jButton3");
+
+        jButton4.setText("jButton4");
+
+        jButton5.setText("jButton5");
+
+        labelPP.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
+        labelPP.setText("PP:");
+        labelPP.setAlignmentY(0.0F);
+
+        jButton6.setText("jButton5");
+
+        jLabel6.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
+        jLabel6.setText("Ataque");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton6)
+                        .addGap(30, 30, 30))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(labelPP)
+                            .addComponent(jLabel6))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
-        telaBatalhaLayout.setVerticalGroup(
-            telaBatalhaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(labelPP, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(13, 13, 13)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30))
         );
+
+        telaBatalha.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, 720, 160));
+
+        jLabel3.setText("jLabel3");
+        jLabel3.setMaximumSize(new java.awt.Dimension(140, 140));
+        jLabel3.setMinimumSize(new java.awt.Dimension(140, 140));
+        jLabel3.setPreferredSize(new java.awt.Dimension(140, 140));
+        telaBatalha.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 50, 140, 140));
+        telaBatalha.add(jProgressBar2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 80, 200, 20));
+        telaBatalha.add(jProgressBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 260, 200, 20));
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
+        jLabel1.setText("jLabel1");
+        telaBatalha.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 230, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
+        jLabel4.setText("jLabel4");
+        telaBatalha.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 50, -1, -1));
 
         painelPrincipal.add(telaBatalha, "telaBatalha");
 
@@ -219,6 +330,19 @@ public class mainFrame extends javax.swing.JFrame {
         // botaoPlay encaminha a tela para o dashboard
         trocarTela("telaDashBoard");
     }//GEN-LAST:event_botaoPlayActionPerformed
+
+    private void botaoPartidaRapidaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoPartidaRapidaActionPerformed
+        // botaoPartidaRapida gera e encaminha para uma partida aleatória
+        trocarTela("telaBatalha");
+    }//GEN-LAST:event_botaoPartidaRapidaActionPerformed
+
+    private void botaoTorneiosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoTorneiosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_botaoTorneiosActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -258,14 +382,29 @@ public class mainFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botaoGaloDex;
     private javax.swing.JButton botaoHelp;
+    private javax.swing.JButton botaoLoja;
+    private javax.swing.JButton botaoPartidaRapida;
     private javax.swing.JButton botaoPerfil;
     private javax.swing.JButton botaoPlay;
     private javax.swing.JButton botaoSettings;
+    private javax.swing.JButton botaoTorneios;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JProgressBar jProgressBar2;
+    private javax.swing.JLabel labelBackgroundDashBoard;
     private javax.swing.JLabel labelBackgroundTelaInicial;
+    private javax.swing.JLabel labelPP;
     private javax.swing.JButton menuInicial;
     private javax.swing.JPanel painelPrincipal;
     private javax.swing.JPanel telaBatalha;
